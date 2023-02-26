d11315402fd7ed4d4fbbbc82f5e9db02d48e0294
akshayarya3696 <83695079+akshayarya3696@users.noreply.github.com>
2023-02-26 13:25:31 +0530

    Create chatbot_response.js

---
+++ chatbot_response.js
@@ -0,0 +1,6 @@
+const responseObj = {
+    hello: "Hey ! welcome to our krishi sahayak family?",
+    hey: "Hey! how can i help you",
+    today: new Date().toDateString(),
+    time: new Date().toLocaleTimeString(),
+  };

fe9960af29482ff7761b8a8d1214acf6f465b291
Nikhil Mathur <86379900+Mathurshab2210@users.noreply.github.com>
2023-05-03 21:52:52 +0530

    Add files via upload

---
+++ javascripts/prog.js
@@ -0,0 +1,164 @@
+let progressBar = document.querySelector(".circular-progress");
+let valueContainer = document.querySelector(".value-container");
+
+let progressValue = 0;
+let progressEndValue = 60;
+let speed = 50;
+
+let progress = setInterval(() => {
+  progressValue++;
+  valueContainer.textContent = `${progressValue}%`;
+  progressBar.style.background = `conic-gradient(
+      #30d189 ${progressValue * 3.6}deg,
+      #cadcff ${progressValue * 3.6}deg
+  )`;
+  if (progressValue == progressEndValue) {
+    clearInterval(progress);
+  }
+}, speed);
+let progressBar1 = document.querySelector(".circular-progress1");
+let valueContainer1 = document.querySelector(".value-container1");
+
+let progressValue1 = 0;
+let progressEndValue1 = 80;
+// let speed = 50;
+
+let progress1 = setInterval(() => {
+  progressValue1++;
+  valueContainer1.textContent = `${progressValue1}%`;
+  progressBar1.style.background = `conic-gradient(
+      #30d189 ${progressValue1 * 3.6}deg,
+      #cadcff ${progressValue1 * 3.6}deg
+  )`;
+  if (progressValue1 == progressEndValue1) {
+    clearInterval(progress1);
+  }
+}, speed);
+let progressBar2 = document.querySelector(".circular-progress2");
+let valueContainer2 = document.querySelector(".value-container2");
+
+let progressValue2 = 0;
+let progressEndValue2 = 80;
+// let speed = 50;
+
+let progress2 = setInterval(() => {
+  progressValue2++;
+  valueContainer2.textContent = `${progressValue2}%`;
+  progressBar2.style.background = `conic-gradient(
+      #30d189 ${progressValue2 * 3.6}deg,
+      #cadcff ${progressValue2 * 3.6}deg
+  )`;
+  if (progressValue2 == progressEndValue2) {
+    clearInterval(progress2);
+  }
+}, speed);
+let progressBar3 = document.querySelector(".circular-progress3");
+let valueContainer3 = document.querySelector(".value-container3");
+
+let progressValue3 = 0;
+let progressEndValue3 = 80;
+// let speed = 50;
+
+let progress3 = setInterval(() => {
+  progressValue3++;
+  valueContainer3.textContent = `${progressValue3}%`;
+  progressBar3.style.background = `conic-gradient(
+      #4d5bf9 ${progressValue3 * 3.6}deg,
+      #cadcff ${progressValue3 * 3.6}deg
+  )`;
+  if (progressValue3 == progressEndValue3) {
+    clearInterval(progress3);
+  }
+}, speed);
+let progressBar4 = document.querySelector(".circular-progress4");
+let valueContainer4 = document.querySelector(".value-container4");
+
+let progressValue4 = 0;
+let progressEndValue4 = 80;
+// let speed = 50;
+
+let progress4 = setInterval(() => {
+  progressValue4++;
+  valueContainer4.textContent = `${progressValue4}%`;
+  progressBar4.style.background = `conic-gradient(
+      #4d5bf9 ${progressValue4 * 3.6}deg,
+      #cadcff ${progressValue4 * 3.6}deg
+  )`;
+  if (progressValue4 == progressEndValue4) {
+    clearInterval(progress4);
+  }
+}, speed);
+
+// ------chatbot-----
+
+const chatBody = document.querySelector(".chat-body");
+const txtInput = document.querySelector("#txtInput");
+const send = document.querySelector("#send");
+
+send.addEventListener("click", () => renderUserMessage());
+
+txtInput.addEventListener("keyup", (event) => {
+  if (event.keyCode === 13) {
+    renderUserMessage();
+  }
+});
+
+const renderUserMessage = () => {
+  const userInput = txtInput.value;
+  renderMessageEle(userInput, "user");
+  txtInput.value = "";
+  setTimeout(() => {
+    renderChatbotResponse(userInput);
+    setScrollPosition();
+  }, 600);
+};
+
+const renderChatbotResponse = (userInput) => {
+  const res = getChatbotResponse(userInput);
+  renderMessageEle(res);
+};
+
+const renderMessageEle = (txt, type) => {
+  let className = "user-message";
+  if (type !== "user") {
+    className = "chatbot-message";
+  }
+  const messageEle = document.createElement("div");
+  const txtNode = document.createTextNode(txt);
+  messageEle.classList.add(className);
+  messageEle.append(txtNode);
+  chatBody.append(messageEle);
+};
+
+const getChatbotResponse = (userInput) => {
+  return responseObj[userInput] == undefined
+    ? "Please try something else"
+    : responseObj[userInput];
+};
+
+const setScrollPosition = () => {
+  if (chatBody.scrollHeight > 0) {
+    chatBody.scrollTop = chatBody.scrollHeight;
+  }
+};
+
+const responseObj = {
+  hello: "Hey ! welcome to our krishi sahayak family?",
+  hey: "Hey! how can i help you",
+  today: new Date().toDateString(),
+  time: new Date().toLocaleTimeString(),
+};
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
